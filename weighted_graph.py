75ff0d692557b6d807de203d9ed5cde628ddf98a
IgoRix08 <igorixmc@gmail.com>
2024-04-05 20:16:22 +0200

    Dodanie klasy z Algorytmem Edmounda-karpa i uzupełnienie funkcjonalnośći klasy weighted_graph

--- weighted_graph.py
+++ weighted_graph.py
@@ -1,12 +1,13 @@
 import dataclasses
 from typing import Dict, Set, Tuple, Union, List
 
+
 @dataclasses.dataclass()
 class Edge():
     first: Union[int, str]
     second: Union[int, str]
-    capacity: int    # przepustowosc         
-    flow: int        # przeplyw          
+    capacity: int  # przepustowosc
+    flow: int  # przeplyw
 
     def __str__(self):
         return f'pierwszy wierzcholek: {self.first}, drugi wierzcholek: {self.second}, przepustowosc: {self.capacity}, przeplyw: {self.flow}'
@@ -16,20 +17,22 @@ class Graph:
     def __init__(self, edges: List[Edge] = []):
         self.edges = edges
         self.node_list: Set[str] = set()
-        self.adjacency_matrix: List[Tuple[int, int]] = []
-        self.adjacency_list: List[str] = {}
-        self.nodes_to_numbers: Dict[str, int] = {}
-        self.numbers_to_nodes: Dict[int, str] = {} # to nigdzie nie jest uzywane (narazie?)
+        self.adjacency_matrix = []
+        self.adjacency_list = {}
+        self.nodes_to_numbers = {}
+        self.numbers_to_nodes = {}
         self.__unpack_nodes()
         self.__encode_nodes()
 
     """Wewnętrzna funkcja. Zapsiuje na set wszystkie wierzcholki w grafie."""
+
     def __unpack_nodes(self):
         for edge in self.edges:
             self.node_list.add(edge.first)
             self.node_list.add(edge.second)
 
     """Wewnetrzna funkcja. Zamienia node'y z str na liczbe naturalna i przechowuje info w odpowiednich slownikach"""
+
     def __encode_nodes(self):
         counter = 0
 
@@ -41,29 +44,18 @@ class Graph:
             counter += 1
 
     """Dodaje nowa krawedz do grafu"""
+
     def add_new_connection(self, edge: Edge):
         self.edges.append(edge)
         self.node_list.add(edge.first)
         self.node_list.add(edge.second)
 
     """Wypisuje wszystkie krawedzie w grafie wraz z wagami"""
+
     def print_all_edges(self):
         for edge in self.edges:
             print(edge)
 
-    def print_all_nodes(self):
-        print(f'Wszystkie wierzcholki w grafie ({len(self.node_list)}): ', end="")
-        print(" ".join(sorted(self.node_list)))
-    
-    """Wypelnia liste sasiedztwa (bez informacji o przeplywie i przepustowosci)"""
-    def create_adjacency_list(self):
-        for node in self.node_list:
-            self.adjacency_list[node] = []
-        
-        for edge in self.edges:
-            self.adjacency_list[edge.first].append(edge.second)
-
-    """Na podstawie macierzy sasiedztwa aktualizuje listę sąsiedztwa i listę połączeń"""
     def update_structure(self):
         index = len(self.adjacency_matrix)
         for i in range(index):
@@ -75,32 +67,47 @@ class Graph:
         for i in range(index):
             for j in range(index):
                 if self.adjacency_matrix[i][j][0] > 0:
-                    edge1 = Edge(self.numbers_to_nodes[i],self.numbers_to_nodes[j],self.adjacency_matrix[i][j][0],self.adjacency_matrix[i][j][1])
+                    edge1 = Edge(self.numbers_to_nodes[i], self.numbers_to_nodes[j],self.adjacency_matrix[i][j][0], self.adjacency_matrix[i][j][1])
                     self.edges.append(edge1)
         self.create_adjacency_list()
 
+    def print_all_nodes(self):
+        print(f'Wszystkie wierzcholki w grafie ({len(self.node_list)}): ', end="")
+        print(" ".join(sorted(self.node_list)))
 
+    """Wypelnia liste sasiedztwa (bez informacji o przeplywie i przepustowosci)"""
+
+    def create_adjacency_list(self):
+        for node in self.node_list:
+            self.adjacency_list[node] = []
+
+        for edge in self.edges:
+            self.adjacency_list[edge.first].append(edge.second)
 
     """Wypelnia macierz sasiedztwa (z informacja o przeplywie i przepustowosci)"""
+
     def create_adjacency_matrix(self):
         self.adjacency_matrix = [[[0, 0] for x in range(len(self.node_list))] for y in range(len(self.node_list))]
 
         for edge in self.edges:
-            self.adjacency_matrix[self.nodes_to_numbers[edge.first]][self.nodes_to_numbers[edge.second]] = [edge.capacity, edge.flow]
-            self.adjacency_matrix[self.nodes_to_numbers[edge.second]][self.nodes_to_numbers[edge.first]] = [edge.flow, edge.capacity]
+            self.adjacency_matrix[self.nodes_to_numbers[edge.first]][self.nodes_to_numbers[edge.second]] = [
+                edge.capacity, edge.flow]
+            #self.adjacency_matrix[self.nodes_to_numbers[edge.second]][self.nodes_to_numbers[edge.first]] = [edge.flow,edge.capacity]
 
     """Wypisuje macierz sasiedztwa (z informacja o przeplywie i przepustowosci)"""
+
     def print_adjacency_matrix(self):
-        for row in graph.adjacency_matrix:
+        for row in self.adjacency_matrix:
             print(row)
 
         print()
 
     def print_all_connections(self):
-        for node in sorted(graph.node_list):
-            print(node, graph.adjacency_list[node])
+        for node in sorted(self.node_list):
+            print(node, self.adjacency_list[node])
 
     """Funkcja ustawi nowa wartosc przeplywu na podanej sciezce (sciezka taka jaka zwraca bfs)"""
+
     def update_flow_on_path(self, path, new_flow):
         index = len(path) - 1
 
@@ -108,7 +115,7 @@ class Graph:
             begin = path[index]
             index -= 1
             end = path[index]
-            index -=1
+            index -= 1
 
             self.adjacency_matrix[self.nodes_to_numbers[begin]][self.nodes_to_numbers[end]][1] = new_flow
             self.adjacency_matrix[self.nodes_to_numbers[end]][self.nodes_to_numbers[begin]][0] = new_flow
@@ -138,7 +145,8 @@ if __name__ == "__main__":
     graph.print_adjacency_matrix()
 
     graph.print_all_connections()
-    
+
     graph.update_flow_on_path(['B', 'C', 'A'], 10)
 
     graph.print_adjacency_matrix()
+
