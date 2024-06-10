106040ba3639a8c43ed7eda8dc8f65ea2351710a
Michalina Lasek <meivels@nyx.re>
2024-06-10 16:59:52 +0200

    Dodano generator danych do problemu 3.

--- towers.py
+++ towers.py
@@ -1,6 +1,9 @@
 import dataclasses
 import random
 from typing import List
+
+import names
+
 import chans_algorithm as chan
 from intersection import Point
 
@@ -9,6 +12,11 @@ class Tower:
     x: int
     y: int
     brightness: int
+
+@dataclasses.dataclass(frozen=True)
+class Flatlander:
+    name: str
+    stamina: int
 
 # tymczasowa funkcja, do usuniecia
 def generate_towers(number_of_towers, number_of_flatlanders, rest_frequency, range_min, range_max):
@@ -20,7 +28,7 @@ def generate_towers(number_of_towers, number_of_flatlanders, rest_frequency, ran
     print(rest_frequency)
 
 # przyjmuje punkty, tworzy otoczke wypukla i generuje dla kazdego jej punktu jasnosc
-def generate_brightness(points: List[Point], range_min: int, range_max: int) -> List[Tower]:
+def generate_brightness(points: List[Point], range_min, range_max: int) -> List[Tower]:
     convex_hull_points = chan.calculate_hull(points)['convex_hull']
     towers = []
 
@@ -37,7 +45,7 @@ def print_towers_data(towers: List[Tower]):
         print(tower.x, tower.y, tower.brightness)
 
 # zapisuje dane wiezy do pliku .txt
-def save__towers_data_to_file(towers: List[Tower]):
+def save_towers_data_to_file(towers: List[Tower]):
     with open('towers_data.py', 'w') as file:
         file.write(str(len(towers)))
         file.write("\n")
@@ -46,6 +54,38 @@ def save__towers_data_to_file(towers: List[Tower]):
             line = str(tower.x) + " " + str(tower.y) + " " + str(tower.brightness)
             file.write(line)
             file.write("\n")
+
+def generate_flatlanders(flatlanders_number, min_stamina, max_stamina: int) -> List[Flatlander]:
+    flatlanders = []
+
+    for i in range(flatlanders_number):
+        flatlanders.append(Flatlander(names.get_first_name(), random.randint(min_stamina, max_stamina)))
+
+    return flatlanders
+
+def generate_problem3_data(flatlanders_number, rest_frequency, tower_range_min, tower_range_max, flatlander_range_min, flatlander_range_max):
+    random_points_num = 100
+    points_min_range = 0
+    points_max_range = 200
+
+    random_points = chan.generate_random_points(random_points_num, points_min_range, points_max_range)
+    towers = generate_brightness(random_points, tower_range_min, tower_range_max)
+    flatlanders = generate_flatlanders(flatlanders_number, flatlander_range_min, flatlander_range_max)
+
+    with open('problem3_data.py', 'w') as file:
+        file.write(str(len(towers)))
+        file.write("\n")
+
+        for tower in towers:
+            file.write(str(tower.x) + " " + str(tower.y) + " " + str(tower.brightness) + "\n")
+
+        file.write(str(flatlanders_number))
+        file.write("\n")
+
+        for flatlander in flatlanders:
+            file.write(flatlander.name + " " + str(flatlander.stamina) + "\n")
+
+        file.write(str(rest_frequency))
 
 if __name__ == "__main__":
     # generate_towers(25, 3, 5, 1, 100)
@@ -57,4 +97,8 @@ if __name__ == "__main__":
     towers2 = generate_brightness(chan.generate_random_points(50, 0, 100), 0, 100)
     # print(towers2)
 
-    save__towers_data_to_file(towers2)
+    # print(generate_flatlanders(10, 3, 0, 10))
+
+    save_towers_data_to_file(towers2)
+
+    generate_problem3_data(25, 4, 0, 100, 1, 15)
